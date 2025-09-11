0cea9a1f701bc0a9c856e5b10fc159a5c9529521
MingrHu <1696994159@qq.com>
2025-09-11 21:23:28 +0800

    change_to_f2024_test

--- test/concurrency/common_checker.h
+++ test/concurrency/common_checker.h
@@ -1,15 +1,3 @@
-//===----------------------------------------------------------------------===//
-//
-//                         BusTub
-//
-// common_checker.h
-//
-// Identification: test/concurrency/common_checker.h
-//
-// Copyright (c) 2015-2025, Carnegie Mellon University Database Group
-//
-//===----------------------------------------------------------------------===//
-
 #pragma once
 
 #include <fmt/format.h>
