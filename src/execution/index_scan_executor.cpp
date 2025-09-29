f559bb425898bfe93c896bfae249e98b4c813510
MingrHu <1696994159@qq.com>
2025-09-29 11:10:20 +0800

    macos

--- src/execution/index_scan_executor.cpp
+++ src/execution/index_scan_executor.cpp
@@ -10,13 +10,53 @@
 //
 //===----------------------------------------------------------------------===//
 #include "execution/executors/index_scan_executor.h"
+#include "storage/index/index_iterator.h"
+#include "storage/table/table_iterator.h"
+#include "storage/table/tuple.h"
+#include "type/value.h"
 
 namespace bustub {
 IndexScanExecutor::IndexScanExecutor(ExecutorContext *exec_ctx, const IndexScanPlanNode *plan)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx),plan_(plan) {}
 
-void IndexScanExecutor::Init() { throw NotImplementedException("IndexScanExecutor is not implemented"); }
+void IndexScanExecutor::Init() { 
+  auto table_info = exec_ctx_->GetCatalog()->GetTable(plan_->table_oid_);
+  auto index_info = exec_ctx_->GetCatalog()->GetIndex(plan_->index_oid_);
 
-auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+  if(!plan_->pred_keys_.empty()){
+    point_search_ = true;
+    // 尝试根据常量表达式获取索引的键
+    // 索引的键本质上也是元组 根据一个或多个列的模式来规定元组
+    std::vector<Value> values;
+    for(const auto& exp:plan_->pred_keys_){
+        // 这里的schema必须是索引的键schema
+        values.emplace_back(exp->Evaluate({},index_info->key_schema_));
+    }
+    Tuple index_key = {values,&index_info->key_schema_};
+    // ScanKey 方法主要是根据索引键进行单点查询 查询的结果放入result 一般只有一个RID
+    index_info->index_->ScanKey(index_key,&point_search_res_ , exec_ctx_->GetTransaction());
+    cur_pos = 0;
+  }
+  else{
+    point_search_ = false;
+    auto tree = dynamic_cast<BPlusTreeIndexForTwoIntegerColumn *>(index_info->index_.get());
+    iter_ = tree->GetBeginIterator();
+  }
+}
+
+auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+  if(point_search_){
+    while(cur_pos < point_search_res_.size()){
+      
+    }
+  }
+  else{
+    // 参考seqscan
+    while(!iter_.IsEnd()){
+
+    }
+  }
+  return false;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/index_scan_executor.h
+++ src/include/execution/executors/index_scan_executor.h
@@ -12,12 +12,17 @@
 
 #pragma once
 
+#include <memory>
 #include <vector>
 
 #include "common/rid.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/index_scan_plan.h"
+#include "storage/index/b_plus_tree_index.h"
+#include "storage/index/extendible_hash_table_index.h"
+#include "storage/index/index_iterator.h"
+#include "storage/index/int_comparator.h"
 #include "storage/table/tuple.h"
 
 namespace bustub {
@@ -25,7 +30,7 @@ namespace bustub {
 /**
  * IndexScanExecutor executes an index scan over a table.
  */
-
+// 要么点查询 要么范围扫描
 class IndexScanExecutor : public AbstractExecutor {
  public:
   /**
@@ -43,6 +48,16 @@ class IndexScanExecutor : public AbstractExecutor {
 
  private:
   /** The index scan plan node to be executed. */
+  /* 对于IndexScanplanNode的成员
+    table_oid_：要扫描的表的标识符
+    index_oid_：要使用的索引的标识符
+    filter_predicate_：可选的过滤谓词表达式，用于在索引扫描时过滤数据
+    pred_keys_：用于点查找的常量值表达式向量（例如 WHERE 子句中的具体值）
+  */
   const IndexScanPlanNode *plan_;
+  bool point_search_;
+  std::vector<RID> point_search_res_;
+  IndexIterator<IntegerKeyType_BTree, IntegerValueType_BTree, IntegerComparatorType_BTree> iter_;
+  size_t cur_pos;
 };
 }  // namespace bustub
